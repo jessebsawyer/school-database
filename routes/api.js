2f4d8ba2bca75e2673fc74c635514760299c86f4
jessebsawyer <46061645+jessebsawyer@users.noreply.github.com>
2020-09-24 19:23:44 -0400

    update

--- routes/api.js
+++ routes/api.js
@@ -4,30 +4,91 @@ const router = express.Router();
 const db = require('../config/database');
 const User = require('../models/User');
 const Course = require('../models/Course');
-const Sequelize = require('sequelize');
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
+const {check, validationResult} = require('express-validator');
 
+const authenticateUser = (req, res, next) => {
+    let message = null;
+    const credentials = auth(req);
+
+    if (credentials) {
+        User.findAll()
+            .then(user => {
+                user.emailAddress === credentials.name
+                if (user) {
+                    const authenticated = bcryptjs
+                        .compareSync(credentials.pass, user.password);
+                    if (authenticated) {
+                        console.log(`Authentication successful for username: ${user.emailAddress}`);
+                        req.currentUser = user;
+                    } else {
+                        message = `Authentication failure for email: ${user.emailAddress}`;
+                    }
+                } else {
+                    message = `User not found for email: ${credentials.name}`;
+                }
+            });
+            
+    } else {
+        message = `Auth header not found`;
+    }
+
+    if (message) {
+        console.warn(message);
+        res.status(401).json({message: 'Access Denied'});
+    } else {
+        next();
+    }
+
+}
 
 // Show All Users
-router.get('/users', async (req, res, next) => {
-    try {
-        const user = await User.findAll();
-        res.json(user)
-    } catch (error) {
-        res.json({message: error.message})
-        next(error);
-    }
+router.get('/users', authenticateUser, (req, res) => {
+    const user = req.currentUser;
+
+    res.json({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailAddress: user.emailAddress,
+        password: user.password
+
+    });
 });
 
 // Create User
-router.post('/users', async (req, res, next) => {
+router.post('/users', [
+    check('firstName')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "firstName"'),
+  check('lastName')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "lastName"'),
+  check('emailAddress')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "email"'),
+  check('password')
+    .exists({checkNull: true, checkFalsy: true})
+    .withMessage('Please provide a password')   
+], async (req, res, next) => {
     try {
-        const user = await User.create({
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => error.msg);
+            return res.status(400).json({ errors: errorMessages });
+        }
+
+        const user = req.body;
+        user.password = bcryptjs.hashSync(user.password);
+
+        const User = await User.create({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             emailAddress: req.body.emailAddress,
             password: req.body.password
         });
-        res.json(user);
+        return res.status(201).end();
     } catch (error) {
         res.json({message: error.message})
         next(error);
