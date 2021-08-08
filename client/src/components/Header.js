819855ce077034c0caf6ac3d7825beaa071f6a82
jessebsawyer <46061645+jessebsawyer@users.noreply.github.com>
2021-08-08 13:14:48 -0400

    add state to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Context from '../context/context';
 
-const Header = () => (
-    <div className="header">
-        <div className="bounds">
-            <h1 className="header--logo">Courses</h1>
-            <nav>
-                <NavLink className="signup" to="/signup">Sign Up</NavLink>
-                <NavLink className="signin" to="/signin">Sign In</NavLink>
-            </nav>
-        </div>
+const Header = (props) => {
+  const userSignInContext = useContext(Context);
+  const { user, isAuthenticated, userLogout, error } = userSignInContext;
+  return (
+    <div className='header'>
+      <div className='bounds'>
+        <h1 className='header--logo'>Courses</h1>
+        <nav>
+          <NavLink className='signup' to='/signup'>
+            Sign Up
+          </NavLink>
+          <NavLink className='signin' to='/signin'>
+            Sign In
+          </NavLink>
+        </nav>
+      </div>
     </div>
-    
-)
+  );
+};
 
 export default Header;
